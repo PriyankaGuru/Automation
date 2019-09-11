4f5a721cb28ad2a3ab558668dca966610706f947
pgurubha <priyanka_gurubhaiye@bmc.com>
2019-09-11 23:03:50 +0530

    Adding FlipkartSearchTest with validations

--- src/test/java/FlipkartSearch.java
+++ src/test/java/FlipkartSearch.java
@@ -1,28 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SearchFlipkart{
+public class SearchFlipkart {
 
-    public SearchFlipkart()
-    {
+    public SearchFlipkart() {
 
     }
 
     @BeforeClass
-    public void launchBrowser()
-    {
+    public void launchBrowser() {
         System.setProperty("webdriver.chrome.driver", "C:\\browser\\chrome.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-   }
+    @Test
+    public void searchOnFlipkartAndValidateResults() {
+        String expectedLapotopString = "Lenovo Ideapad 130 Core i3 7th Gen - (4 GB/1 TB HDD/Windows 10 Home) 130-15IKB Laptop";
+        driver.get("http://www.flipkart.com/");
+        WebDriverWait wait = new WebDriverWait(driver, 40);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title="Search for products, brands
+        and more "]")));
+
+        Actions act = new Actions(driver);
+        WebElement mainmenu = driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[2]/div/ul/li[1]"));
+        act.moveToElement(mainmenu).build().perform();//taking mouse
+        WebElement mobiles = driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[2]/div/ul/li[1]/ul/li/ul/li[1]/ul/li[1]/a"));
+        mobiles.click();//clicking on mobiles
+
+        driver.findElement(By.xpath("//div[@class='O8ZS_U']/input")).sendKeys("laptops");
+        driver.findElement(By.xpath("//div[@class='O8ZS_U']/input")).sendKeys(Keys.ENTER);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@title='HP']//div[@class='_1p7h2j']");
+        WebElement laptopElement = driver.findElemet(By.xpath("//div[@class='_3wU53n']"));
+        boolean isElementDisplayed = laptopElement.isDisplayed();
+        Assert.assertEquals(isElementDisplayed, true);
+        Assert.assertEquals(laptopElement.getText()), expectedLapotopString);
+    }
+}
